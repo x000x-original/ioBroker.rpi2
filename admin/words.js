dc30cf6bec6d48b57584fff8b48bb95e802cb22a
Ingo Fischer <github@fischer-ka.de>
2018-01-02 00:57:06 +0100

    split admin translations into separate file

---
+++ admin/words.js
@@ -0,0 +1,23 @@
+systemDictionary = {
+    "RPI-Monitor Adapter settings": {
+        "en": "RPI-Monitor Adapter settings",
+        "de": "RPI-Monitor Adapter-Einstellungen",
+        "ru": "Настройки RPI-монитор адаптера"
+    },
+    "CPU:":         {"en": "CPU:",          "de": "CPU:",       "ru": "Процессор:"},
+    "Memory:":      {"en": "Memory:",       "de": "Speicher:",  "ru": "Память"},
+    "Raspberry:":   {"en": "Raspberry:",    "de": "Raspberry:", "ru": "Raspberry"},
+    "Network:":     {"en": "Network:",      "de": "Network:",   "ru": "Сеть"},
+    "SDCard:":      {"en": "SD Card:",      "de": "SD Karte:",  "ru": "SD Карта"},
+    "Swap:":        {"en": "Swap:",         "de": "Swap:",      "ru": "Swap"},
+    "Temperature:": {"en": "Temperature:",  "de": "Temperatur:", "ru": "Температура"},
+    "Uptime:":      {"en": "Uptime:",       "de": "Uptime:",    "ru": "Uptime"},
+    "WLAN:":        {"en": "WLAN:",         "de": "WLAN:",      "ru": "WLAN"},
+    "out":          {"en": "out",           "de": "out",        "ru": "out"},
+    "in":           {"en": "in",            "de": "in",         "ru": "in"},
+    "Port":         {"en": "Port",          "de": "Port",       "ru": "Порт"},
+    "Enabled":      {"en": "Enabled",       "de": "Aktiv",      "ru": "Активен"},
+    "Type":         {"en": "Type",          "de": "Typ",        "ru": "Тип"},
+    "Poll":         {"en": "Poll",          "de": "Pollen",     "ru": "Опрашивать"},
+    "GPIOs":        {"en": "GPIOs",         "de": "GPIOs",      "ru": "GPIO Порты"}
+};
